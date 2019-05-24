59d12ed6fe0144766a6a8eab31c584b2a6c15a2e
rahulsunil2 <rahulsunil2@gmail.com>
2019-05-25 00:55:05 +0530

    Hello World from PC

---
+++ src/new_main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    print("Hello World")
+    print("Hello World From PC")
